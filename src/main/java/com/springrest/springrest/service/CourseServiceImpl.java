1f9600092887e30bb16a8f3bfed8a566a1874bec
Maharana Singh <maharana.singh@juspay.in>
2024-03-18 15:18:03 +0530

    quickpay change

--- src/main/java/com/springrest/springrest/service/CourseServiceImpl.java
+++ src/main/java/com/springrest/springrest/service/CourseServiceImpl.java
@@ -5,66 +5,76 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.springrest.springrest.dao.CourseDao;
 import com.springrest.springrest.entities.Course;
 
 
 @Service
 public class CourseServiceImpl implements Courseservice {
 	
-	List<Course> list;
+//	List<Course> list;
+	@Autowired
+	private CourseDao courseDao;
 	
 	public CourseServiceImpl() {
-		list = new ArrayList<>();
-		list.add(new Course(14,"hindi","Hindi books"));
-		list.add(new Course(145,"java core","java core des"));
+//		list = new ArrayList<>();
+//		list.add(new Course(14,"hindi","Hindi books"));
+//		list.add(new Course(145,"java core","java core des"));
 	}
 
 	@Override
 	public List<Course> getCourses() {
 		// TODO Auto-generated method stub
-		return list;
+//		return list;
+		return courseDao.findAll();
 	}
 
 	@Override
 	public Course getCourse(long courseId) {
 		// TODO Auto-generated method stub
-		Course c=null;
-		for(Course course:list) {
-			if(course.getId()==courseId) {
-				c=course;
-				break;
-			}
-		}
-		return c;
+//		Course c=null;
+//		for(Course course:list) {
+//			if(course.getId()==courseId) {
+//				c=course;
+//				break;
+//			}
+//		}
+//		return c;
+		return courseDao.getOne(courseId);
 	}
 
 	@Override
 	public Course addCourse(Course course) {
 		// TODO Auto-generated method stub
-		list.add(course);
-		return course;
+//		list.add(course);
+//		return course;
+		return courseDao.save(course);
 	}
 
 	@Override
 	public Course updateCourse(Course course) {
 		// TODO Auto-generated method stub
-		for(Course e:list) {
-			if(e.getId() == course.getId()) {
-				e.setTitle(course.getTitle());
-				e.setDescription(course.getDescription());
-			}
-		};
+//		for(Course e:list) {
+//			if(e.getId() == course.getId()) {
+//				e.setTitle(course.getTitle());
+//				e.setDescription(course.getDescription());
+//			}
+//		};
 
-		return course;
+//		return course;
+		return courseDao.save(course);
 	}
 
 	@Override
 	public void deleteCourse(long courseId) {
-		list=this.list.stream().filter(e->e.getId()!=courseId).collect(Collectors.toList());
+//		list=this.list.stream().filter(e->e.getId()!=courseId).collect(Collectors.toList());
+		Course entity= courseDao.getOne(courseId);
+		courseDao.delete(entity);
 	}
 
 }
